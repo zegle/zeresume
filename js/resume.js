0a7458333e210914bd9e8ae93cd2cac1a416d1c3
zachary egle <35083488+zewebdesign@users.noreply.github.com>
2018-03-16 21:42:20 -0600

    Add files via upload

--- js/resume.js
+++ js/resume.js
@@ -8,31 +8,106 @@ $(document).ready(function () {
 
 });
 
+/*..................Page animation triggers when element comes into viewport...............*/
+  (function ($) {
+
+    /**
+     * Copyright 2012, Digital Fusion
+     * Licensed under the MIT license.
+     * http://teamdf.com/jquery-plugins/license/
+     *
+     * @author Sam Sehnert
+     * @desc A small plugin that checks whether elements are within
+     *     the user visible viewport of a web browser.
+     *     only accounts for vertical position, not horizontal.
+     */
+
+    $.fn.visible = function (partial) {
+
+      var $t = $(this),
+        $w = $(window),
+        viewTop = $w.scrollTop(),
+        viewBottom = viewTop + $w.height(),
+        _top = $t.offset().top,
+        _bottom = _top + $t.height(),
+        compareTop = partial === true ? _bottom : _top,
+        compareBottom = partial === true ? _top : _bottom;
+
+      return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+    };
+
+  })(jQuery);
+
+var win = $(window);
+
+win.scroll(function (event) {
+
+  $(".animated").each(function (i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("fadeLeftRight");
+      $(".showhide").css('visibility', 'visible');
+    }
+    else {
+      $(".animated").removeClass("fadeLeftRight");
+      $(".showhide").css('visibility', 'hidden');
+    }
+  });
+  $(".experience").each(function (i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      $(".animate1").addClass("slidelft").css('visibility', 'visible');
+      $(".animate2").addClass("slidedwn").css("visibility", "visible");
+      $(".animate3").addClass("slideup").css("visibility", "visible");
+    }
+    else {
+      $(".animate1").removeClass("slidelft").css("visibility", "hidden");
+      $(".animate2").removeClass("slidedwn").css("visibility", "hidden");
+      $(".animate3").removeClass("slideup").css("visibility", "hidden");
+    }
+  });
+  $(".education").each(function (i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      $(".animate4").addClass("slidelft").css('visibility', 'visible');
+      $(".animate5").addClass("slidedwn").css("visibility", "visible");
+      $(".animate6").addClass("slideup").css("visibility", "visible");
+    }
+    else {
+      $(".animate4").removeClass("slidelft").css("visibility", "hidden");
+      $(".animate5").removeClass("slidedwn").css("visibility", "hidden");
+      $(".animate6").removeClass("slideup").css("visibility", "hidden");
+    }
+  });
+  $(".skills").each(function (i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      $(".animate7").addClass("bounceInLeft").css('visibility', 'visible');
+    }
+    else {
+      $(".animate7").removeClass("bounceInLeft").css("visibility", "hidden");
+    }
+  });
+  
+});
+
 // configured nav slide in at specific scroll point, 
 // adds/removes body padding at specific scroll position
 $(function() {
   $(window).scroll(function () {
+    var availableScroll = $(document).height() - $(window).height();
+      var x = $(window).scrollTop();
     if ($(window).width() >= 992) {
-      if ($(this).scrollTop() >= 998) {
+      if (x >= 998 || Math.abs(x - availableScroll) < 10) {
         $("body").stop().animate({ "padding-left": "17rem" });
         $("#sideNav").stop().animate({ "left": "0px" });
-        $(".animate").addClass("typewriter").css('visibility', 'visible');
+        /*$(".animate").addClass("typewriter").css('visibility', 'visible');*/
       }
       else {
         $("#sideNav").stop().animate({"left":"-300px"});
         $("body").stop().animate({ "padding-left": "0rem" });
-        $(".animate").removeClass("typewriter").css('visibility', 'hidden');
-      }
-      if ($(this).scrollTop() >= 1972) {
-        $(".animate1").addClass("slidelft").css("visibility", "visible");
-        $(".animate2").addClass("slidedwn").css("visibility", "visible");
-        $(".animate3").addClass("slideup").css("visibility", "visible");
-
-      }
-      else {
-        $(".animate1").removeClass("slidelft").css("visibility", "hidden");
-        $(".animate2").removeClass("slidedwn").css("visibility", "hidden");
-        $(".animate3").removeClass("slideup").css("visibility", "hidden");
+        /*$(".animate").removeClass("typewriter").css('visibility', 'hidden');*/
       }
     }
   });
